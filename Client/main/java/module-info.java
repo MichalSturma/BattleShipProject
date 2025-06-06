bf572651e73a08dc36df4a355b76dfe49e3da8d4
stum08 <stum08@vse.cz>
2025-06-06 16:14:32 +0200

    fix

--- Client/main/java/module-info.java
+++ Client/main/java/module-info.java
@@ -2,7 +2,6 @@ module cz.vse.klient {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.logging.log4j;
-    // pokud používáš Log4j2 core, přidej také:
 
 
     opens cz.vse.klient to javafx.fxml;
